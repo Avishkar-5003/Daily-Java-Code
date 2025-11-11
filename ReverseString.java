27649011329a74e5cca57ea5f800141c8eb7039b
Avishkar-5003 <tambeavishkar1@gmail.com>
2025-11-11 14:26:38 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+package com.practise;
+
+public class ReverseString {
+
+	
+	    public static void main(String[] args) {
+	        String str = "Avishkar";
+	        String rev = "";
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            rev += str.charAt(i);
+	        }
+	        System.out.println("Reversed: " + rev);
+	    }
+	}
+
+
+
